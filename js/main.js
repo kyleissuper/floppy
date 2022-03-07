88dfb128dff260585ae1646f5a415ee60febab2f
Kyle Tan <kyle@kyletan.com>
2022-03-06 22:12:29 -0800

    Load font using WebFont

--- js/main.js
+++ js/main.js
@@ -5,8 +5,14 @@ import kitty from "./kitty.js";
 import obstacleMaker from "./obstacleMaker.js";
 import scoreBoard from "./scoreBoard.js";
 import backDrop from "./backDrop.js";
+import "./webfont.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  game.setup(physics, kitty, obstacleMaker, scoreBoard, backDrop);
-  loop.start(game.nextFrame, game.renderWithLag);
+WebFont.load({
+  google: {
+    families: ['Averia Serif Libre:700'],
+  },
+  active() {
+    game.setup(physics, kitty, obstacleMaker, scoreBoard, backDrop);
+    loop.start(game.nextFrame, game.renderWithLag);
+  },
 });
